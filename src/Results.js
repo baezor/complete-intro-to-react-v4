ab2312f6d805bb09c9ab7556917de65d79ddfa65
Angel Baez <angel@uptrending.com>
2019-02-25 23:44:31 -0600

    Context completed

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import pf from 'petfinder-client';
+import { Consumer } from './SearchContext';
 import Pet from './Pet';
+import SearchBox from './SearchBox';
 
 const petfinder = pf({
   key: process.env.API_KEY,
@@ -16,7 +18,15 @@ class Results extends React.Component {
     };
   }
   componentDidMount(){
-    petfinder.pet.find({ output: "full", location: "Seattle, WA" })
+    this.search();
+  }
+  search = () => {
+    petfinder.pet.find({ 
+      output: "full", 
+      location: this.props.searchParams.location,
+      animal: this.props.searchParams.animal,
+      breed: this.props.searchParams.breed
+    })
     .then(data => {
       let pets;
       if(data.petfinder.pets && data.petfinder.pets.pet){
@@ -38,7 +48,8 @@ class Results extends React.Component {
   render() {
     return (
       <div className="search">
-      
+        <SearchBox search={this.search} />
+
           { this.state.pets.map(pet => {
             let breed;
             if(Array.isArray(pet.breeds.breed)){
@@ -65,4 +76,10 @@ class Results extends React.Component {
   }
 }
 
-export default Results; 
+export default function ResultsWithContext(props){
+  return (
+    <Consumer>
+      {context => <Results {...props} searchParams={context} />}
+    </Consumer>
+  )
+}; 
